317e0d336c7914345bb3be6641da3b251d42f94e
Thomas Leskin <tleskin@gmail.com>
2015-02-24 13:04:04 -0700

    Access element by numeric position

--- linked_list.rb
+++ linked_list.rb
@@ -43,11 +43,53 @@ class IterativeLinkedList
     end
     current.data
   end
+
+  def delete(target)
+    if head.data == target
+      @head = nil
+    else
+      current = head
+      last = nil
+      while current.link?
+        last = current
+        current = current.link
+
+        if current.data == target
+          last.link = nil
+        end
+      end
+    end
+  end
+
+  def to_a
+    result = []
+    current = head
+    while current
+      result << current.data
+      current = current.link
+    end
+    return result
+  end
+
 end
 
-#class RecursiveLinkedList
-  # A list using Recursive approaches
-#end
+class RecursiveLinkedList
+
+  attr_reader :head
+
+  def push(data)
+    if head
+      head.push(data)
+    else
+      @head = Node.new
+    end
+  end
+
+  def count
+    head.count
+  end
+
+end
 
 class Node
 
--- linked_list_test.rb
+++ linked_list_test.rb
@@ -47,6 +47,7 @@ class LinkedListTest < Minitest::Test
   def test_a_popped_element_is_removed
     list.push("pizza")
     output = list.pop
+    assert_equal "pizza", output
     assert_equal 0, list.count
   end
 
@@ -56,6 +57,70 @@ class LinkedListTest < Minitest::Test
     list.push("ice cream")
     output = list.pop
     assert_equal output, "ice cream", output
+    assert_equal 2, list.count
+  end
+
+  def test_it_deletes_a_node
+    list.push("pizza")
+    list.delete("pizza")
+    assert_equal 0, list.count
+  end
+
+  def test_it_does_not_delete_node_when_data_doesnt_match
+    list.push("pizza")
+    list.delete("sushi")
+    assert_equal 1, list.count
+  end
+
+  def test_it_deletes_the_last_node
+    list.push("pizza")
+    list.push("sandwich")
+    list.push("ice cream")
+    list.delete("ice cream")
+    assert_equal 2, list.count
+  end
+
+  def test_it_deletes_a_middle_node
+    list.push("pizza")
+    list.push("sandwich")
+    list.push("ice cream")
+    list.delete("sandwich")
+    assert_equal 2, list.count
+  end
+
+  def test_it_converts_to_an_array_when_there_are_no_elements
+    assert_equal [], list.to_a
+  end
+
+  def test_it_converts_to_an_array_with_several_elements
+    list.push("pizza")
+    list.push("kebab")
+    list.push("sandwich")
+    assert_equal ["pizza", "kebab", "sandwich"], list.to_a
+    assert_equal ["pizza", "kebab", "sandwich"], list.to_a
+  end
+
+## These are the rescursive list tests
+
+  def test_it_exists
+    skip
+    assert RecursiveLinkedList
+  end
+
+  def test_it_inserts_an_element
+    skip
+    list = RecursiveLinkedList.new
+    assert_equal 0, list.count
+    list.push("pizza")
+    assert_equal 1, list.count
+  end
+
+  def test_it_inserts_multiple_elements
+    skip
+    list = RecursiveLinkedList.new
+    assert_equal 0, list.count
+    list.push("pizza")
+    list.push("kebab")
     assert_equal 2, list.count
   end
 
